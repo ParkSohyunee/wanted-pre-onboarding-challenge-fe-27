6b16eb314d6f792d5e1cd32c246f8cfc1da44556
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-11-04 13:05:55 +0900

    feat: request 할 때 토큰이 Authorization header에 전달되도록 axios interceptors 설정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,4 +4,18 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
